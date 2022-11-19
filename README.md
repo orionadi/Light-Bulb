55a7d7e03a7a7ccde575c5c1496d8c5fcfcb900c
Aditya <orioninthespace@gmail.com>
2022-11-20 01:29:33 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# turn me on/off
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/orionadi/pen/poPRxda](https://codepen.io/orionadi/pen/poPRxda).
+
---
+++ src/script.js
@@ -0,0 +1,8 @@
+
+function turnon(){
+  document.getElementById("bulb").src="https://www.w3schools.com/js/pic_bulbon.gif";
+}
+
+function turnoff(){
+  document.getElementById("bulb").src="https://www.w3schools.com/js/pic_bulboff.gif";
+}
